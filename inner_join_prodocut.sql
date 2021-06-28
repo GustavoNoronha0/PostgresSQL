1a08a49d098b6831399dc9c15215d7c578b1921e
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-06-28 17:16:10 -0300

    feat: add list relationship with inner let and right join

---
+++ inner_join_prodocut.sql
@@ -0,0 +1,3 @@
+select * from product inner join category on product.category_id = category.id
+select * from product left join category on product.category_id = category.id
+select * from product right join category on product.category_id = category.id
